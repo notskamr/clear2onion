30ceace2d3775d9a1e59f112a4cdf3d2c6a05148
Varun Sahni <sahnivarun62@gmail.com>
2024-03-14 23:27:37 +0530

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://clear2onion.netlify.app",
+  site: "https://clear2onion.pages.dev",
   output: "hybrid",
   adapter: cloudflare(),
   integrations: [tailwind(), svelte()]
