a390d4cc619121e2b0774255b67249f3f6ff03d5
Varun Sahni <sahnivarun62@gmail.com>
2024-03-15 13:41:35 +0530

    Math.random -> node:crypto

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,9 @@
+import { randomInt } from "node:crypto";
 export function generateShortUrl(length: number): string {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     let result = '';
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
+        const randomIndex = randomInt(0, characters.length);
         result += characters.charAt(randomIndex);
     }
     return result;
