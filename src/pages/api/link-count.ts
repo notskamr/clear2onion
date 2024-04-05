4f64b30a50a33cac7d52448eb07958db1b17b0e6
Varun Sahni <sahnivarun62@gmail.com>
2024-04-05 17:29:51 +0530

    Don't prerender api routes

--- src/pages/api/link-count.ts
+++ src/pages/api/link-count.ts
@@ -2,6 +2,8 @@ import { turso } from "@/db";
 import { JSONResponse } from "@/globals/responses";
 import type { APIContext } from "astro";
 
+export const prerender = false;
+
 export async function GET({ }: APIContext) {
     const { rows } = await turso.execute("SELECT COUNT(*) as count FROM urls");
     const count = rows[0].count as number;
